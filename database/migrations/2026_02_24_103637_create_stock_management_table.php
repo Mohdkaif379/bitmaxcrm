645345f85e78c5e51287e540a6412cd02e9a23ed
Mohdkaif379 <mohdkaif8603@gmail.com>
2026-02-24 12:48:38 +0530

    update

--- database/migrations/2026_02_24_103637_create_stock_management_table.php
+++ database/migrations/2026_02_24_103637_create_stock_management_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stock_management', function (Blueprint $table) {
+        Schema::create('stock_managements', function (Blueprint $table) {
             $table->id();
             $table->string('item_name', 255);
             $table->text('description');
