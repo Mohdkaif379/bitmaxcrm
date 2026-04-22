a16a1e9530c78dbc0cc1e400d82c2b9416dbc735
Mohdkaif379 <mohdkaif8603@gmail.com>
2026-04-22 10:22:31 +0530

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,17 +11,17 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         DB::table('admins')->insert([
-            'full_name'     => 'Arman Hossain',
+            'full_name'     => 'Admin',
             'email'         => 'admin@gmail.com',
-            'password'      => Hash::make('12345678'), // 👈 hashed password
-            'number'        => '9027114834',
+            'password'      => Hash::make('12345678'),
+            'number'        => '9027114840',
             'profile_photo' => null,
             'role'          => 'admin',
             'permissions'   => json_encode(['*']),
             'status'        => 1,
-            'bio'           => 'i am backend developer',
+            'bio'           => 'I am the admin of this application.',
             'company_logo'  => null,
-            'company_name'  => 'Kaif Software Pvt Ltd',
+            'company_name'  => 'Bitmax Technology Pvt. Ltd.',
             'created_at'    => now(),
             'updated_at'    => now(),
         ]);
