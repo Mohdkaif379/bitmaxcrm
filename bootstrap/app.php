0aba6c42be28082116bd62b901af38e5bfe0ea28
Mohdkaif379 <mohdkaif8603@gmail.com>
2026-05-08 15:03:43 +0530

    updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'auth.jwt' => \App\Http\Middleware\JwtAuthMiddleware::class,
+            'jwt.auth' => \App\Http\Middleware\JwtAuthMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
